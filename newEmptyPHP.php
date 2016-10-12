0ae23828bb09f7dc447518fd56e1385b07d0034b
Administrator <Administrator@USER-20161009MF>
2016-10-12 09:37:23 +0800

    xxoo

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -4,5 +4,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */echo '111111';
+ */
+    echo '111111';
+    echo '2222222222222';
+    echo '3333333333333';
 
